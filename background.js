678ba49ab006f2976192bc3e71a05ee5de694599
f-vitor <145235878+f-vitor@users.noreply.github.com>
2024-09-21 11:05:40 -0300

    background.js

---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extensão instalada.");
+  });
+  chrome.runtime.onInstalled.addListener(() => {
+    console.log("Service worker ativado. Extensão instalada.");
+  });
